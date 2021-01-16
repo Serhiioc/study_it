0e155bacc654806dde206bdc6332317d5e9d8d03
Serhiioc <kreat1vesc@gmail.com>
2021-01-16 23:39:32 +0200

    random bgc lesson3

---
+++ js_lesson3_bgc_Serhiioc/js/common.js
@@ -0,0 +1,18 @@
+
+// document.addEventListener("DOMcontentloaded", function() {
+   
+
+// });
+
+document.addEventListener("keydown", function (evt) {
+    if (evt.code == "Space") {
+    let hex = '#';
+    let range = 'ABCDEF0123456789';
+
+    for (var i = 0; i < 6; i++ ) {
+      hex += range.charAt(Math.floor(Math.random() * range.length));
+    }
+        document.querySelector("body").style.backgroundColor = hex;
+    } 
+});
+  
