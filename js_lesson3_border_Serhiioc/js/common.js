b9bbad88cb8b48e38ef3c659d5826e8d14b039eb
Serhiioc <kreat1vesc@gmail.com>
2021-01-18 22:32:41 +0200

    Border radius fixed

--- js_lesson3_border_Serhiioc/js/common.js
+++ js_lesson3_border_Serhiioc/js/common.js
@@ -1,13 +1,30 @@
 
-let btl = document.querySelector(".top").value,
-    btr = document.querySelector(".right").value,
-    bbr = document.querySelector(".bottom").value,
-    bbl = document.querySelector(".left").value;
+let btl = document.querySelector(".top"),
+    btr = document.querySelector(".right"),
+    bbr = document.querySelector(".bottom"),
+    bbl = document.querySelector(".left");
 
-document.querySelector(".sample").style.borderRadius= btl+`px `+btr+`px `+bbr+`px `+bbl+`px`;
+// document.querySelector(".sample").style.borderRadius= btl+`px `+btr+`px `+bbr+`px `+bbl+`px`;
 
-document.querySelectorAll(".range").addEventListner("click", function() {
-console.log(btl);
+// document.querySelector(".top").addEventListner("change", function () {
+//         rtl = btl.value
+//         document.querySelector(".sample").style.borderTopLeftRadius = btl+`px`;
+//     });
+ 
 
+btl.addEventListener("change", function () {
+    document.querySelector(".sample").style.borderStartStartRadius= btl.value+`px`;
+});
+
+btr.addEventListener("change", function () {
+    document.querySelector(".sample").style.borderStartEndRadius= btr.value+`px`;
+});
+
+bbr.addEventListener("change", function () {
+    document.querySelector(".sample").style.borderEndStartRadius= bbr.value+`px`;
+});
+
+bbl.addEventListener("change", function () {
+    document.querySelector(".sample").style.borderEndEndRadius= bbl.value+`px`;
 });
 
