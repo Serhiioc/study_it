30f88007ea0f6eb55a2f27eed5ebd45720b5d582
Serhiioc <kreat1vesc@gmail.com>
2021-02-08 22:20:29 +0200

    ajax Form

--- js_lesson9_Serhiioc/js/common.js
+++ js_lesson9_Serhiioc/js/common.js
@@ -4,70 +4,54 @@ $(document).ready(function () {
     let im = new Inputmask("+38 (099) 999-99-99");
     im.mask(phoneFields);
 
-    $.ajax({
-        type: "post",
-        url: "mail.php",
-        beforeSend: ,
-        data: "data",
-        dataType: "dataType",
-        success: function (response) {
-            
-        }
-    });
-
-    new JustValidate('.js-form', {
-        rules: {
-            name: {
-                required: true,
-                minLength: 2
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            phone: {
-                required: true
-            }
-        },
-        messages: {
-            name: {
-                required: "False",
-                minLength: "Короткое имя"
-            },
-            email: {
-                required: "False",
-                email: "Не верю"
-            },
-            phone: {
-                required: "False"
-            }
-        },
     
-        submitHandler: function (form) {
-            let xhr = new XMLHttpRequest();
+    $(".js-form").submit(function (evt) {
+        evt.preventDefault();
 
-            xhr.open("POST", "mail.php", true);
-
-            let FormData = new FormData(form);
-
-            xhr.addEventListener("load", function() {
-                if( xhr.readyState === 4) {
-                    switch (xhr.status) {
-                        case 200:
-                            console.log("Form send");
-                            form.reset();
-                            break;
-                        case 404:
-                            console.log("Warning");
-                            break;
-                        default:
-                        console.log("Error server");
-                        break;
-                    }
+        new JustValidate('.js-form', {
+            rules: {
+                name: {
+                    required: true,
+                    minLength: 2
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                phone: {
+                    required: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "False",
+                    minLength: "Короткое имя"
+                },
+                email: {
+                    required: "False",
+                    email: "Не верю"
+                },
+                phone: {
+                    required: "False"
+                }
+            },
+        });
+    
+            $.ajax({
+                type: "POST",
+                contentType: "application/json; charset=utf-8",
+                url: "mail.php",
+                data: $(".js-form").serialize(),
+                success: function (response) {
+                   console.log(response);
+                   console.log("good");
+                   $(".js-form").reset;
+                },
+                errors: function() {
+                    console.log("bad");
                 }
             });
 
-            xhr.send(FormData);
-        },
-      });
+    });
+    
 });
